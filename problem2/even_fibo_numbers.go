562d59ed733d63085a4395de10aabd75123c6a5a
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-15 12:30:58 -0600

    got program working
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- problem2/even_fibo_numbers.go
+++ problem2/even_fibo_numbers.go
@@ -3,20 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	fibslice := []int{0, 1}
+	limit := 4000000
+	fibo := fibogen(limit)
+	fibeven := fibeven(fibo)
 
-	for range [4000000]struct{}{} {
-		fib(&fibslice)
+	fmt.Println(fibeven[len(fibeven)-1])
+
+}
+
+func fibogen(limit int) []int {
+	var fibo []int
+	var last int
+	i := 1
+
+	for i <= limit {
+		fibo = append(fibo, i)
+		if len(fibo) >= 2 {
+			last = fibo[len(fibo)-2]
+		} else {
+			last = 0
+		}
+		i += last
 	}
-	fmt.Println(fibslice)
+	return fibo
 }
 
-func fib(fibslice *[]int) {
-	s := *fibslice
-	next := s[len(s)-2] + s[len(s)-1]
-	*fibslice = append(s, next)
-}
-
-func iseven(n int) bool {
-	return n%2 == 0
+func fibeven(fibo []int) []int {
+	var fibeven []int
+	for _, v := range fibo {
+		if v%2 == 0 {
+			fibeven = append(fibeven, v)
+		}
+	}
+	return fibeven
 }
