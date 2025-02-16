9d977e241b593f2dcbd603494f0b827342378f2e
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-16 11:46:51 -0600

    completed highest palindrome
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

---
+++ problem4/go.mod
@@ -0,0 +1,3 @@
+module github.com/vtrenton/go-peuler/problem4
+
+go 1.23.2
--- problem4/highestpalendrome.go
+++ problem4/highestpalendrome.go
@@ -1,14 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
 func main() {
 	var pal []int
-	for i := 100; i <= 999; i++ { //what the fuck
-		if isPrime() {
-			for j := 100; j <= 999; j++ {
-				if isPrime() {
-					pal = append(pal, j)
-				}
+
+	// need to do better than o(n^2)
+	for i := 100; i <= 999; i++ {
+		for j := i; j <= 999; j++ {
+			product := i * j
+			if ispalindrome(product) {
+				pal = append(pal, product) //not appending in order
 			}
 		}
 	}
+	sort.Ints(pal)
+	fmt.Println(pal[len(pal)-1])
+}
+
+func ispalindrome(num int) bool {
+	strnum := strconv.Itoa(num)
+	split := (len(strnum) + 2 - 1) / 2
+
+	for i := range split {
+		if strnum[i] != strnum[len(strnum)-1-i] {
+			return false
+		}
+	}
+
+	return true
 }
