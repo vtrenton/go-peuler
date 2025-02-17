31daaa672f40125f84fc26a3f0397592d25ea5af
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-17 13:39:42 -0600

    trying new algorithm
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- problem5/smallestmult.go
+++ problem5/smallestmult.go
@@ -7,26 +7,59 @@ import (
 
 func main() {
 	const limit = 20
-	num := limit
-	var i int
-	for {
-		i++
-		fmt.Printf("i is %d\n", i)
-		if evendivbyall(num, limit) {
-			fmt.Println(num)
-			break
-		}
-		num = int(math.Pow(limit, float64(i)))
-		fmt.Printf("num is %d\n", num)
-	}
-	fmt.Println(num)
+	primes := getprimestolimit(limit)
+	exponents := getexponents(primes, limit)
+	lcm := getlcm(primes, exponents)
+
+	fmt.Println(lcm)
 }
 
-func evendivbyall(num, limit int) bool {
-	for i := 1; i <= limit; i++ { // need to make sure we start at 1
-		if num%i != 0 {
+func getprimestolimit(limit int) []int {
+	var primes []int
+	for i := range limit {
+		if isprime(i) {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
+func isprime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
 			return false
 		}
 	}
 	return true
+}
+
+func getexponents(primes []int, limit int) []int {
+	var exponent int
+	var primeexp []int
+	for prime := range primes {
+		for exponent = 1; int(math.Pow(float64(prime), float64(exponent))) < limit; exponent++ {
+		}
+		primeexp = append(primeexp, exponent)
+	}
+	return primeexp
+}
+
+func getlcm(primes, exponents []int) int {
+	result := 1
+	var multiples []int
+	for prime := range primes {
+		for exp := range exponents {
+			mult := int(math.Pow(float64(prime), float64(exp)))
+			multiples = append(multiples, mult)
+		}
+	}
+
+	for multiple := range multiples {
+		result *= multiple
+	}
+
+	return result
 }
