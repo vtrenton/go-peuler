ce690b39d89ca220c6cf8e8609965aae59fd0735
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-24 22:04:29 -0600

    need to find a method that wont start PC on fire
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- problem12/trianglediv.go
+++ problem12/trianglediv.go
@@ -1,39 +1,33 @@
 package main
 
+import "fmt"
+
 func main() {
+	i := 1
+	for {
+		triangle := calctriagle(i)
+		factors := getfactors(triangle)
+		fmt.Printf("%d: %v\n", triangle, factors)
+		if len(factors) >= 500 {
+			break
+		}
+		i++
+	}
 
 }
-
-// Triangle numbers are in a patter of diff between each number is plus one
-/*
-0
-1 = 1
-3 = 2
-6 = 3
-10
-15
-21
-*/
 
 func calctriagle(size int) int {
-	var triangle int
-	for diff := range size {
-		triangle += diff
+	var triange int
+	for i := range size {
+		triange += i
 	}
+	return triange
 }
-
-//func calctriagle(size int) int {
-//	var triange int
-//	for i := range size {
-//		triange += i
-//	}
-//	return triange
-//}
 
 func getfactors(n int) []int {
 	var factors []int
 	limit := n / 2
-	for i := range limit {
+	for i := 1; i <= limit; i++ {
 		if n%i == 0 {
 			factors = append(factors, i)
 		}
