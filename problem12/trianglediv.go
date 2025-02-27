e29ce65c8a6fe0ecedd4f4098a9632614cfe40de
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-26 18:47:45 -0600

    trying to find a pattern with primes
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- problem12/trianglediv.go
+++ problem12/trianglediv.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	factors := make([]int, 0)
 	for i := 1; len(factors) <= 500; i++ {
 		triangle := calctriagle(i)
 		factors := getfactors(triangle)
-		fmt.Printf("%d: %v\n", triangle, factors)
+		pfactors := primeFilter(factors)
+		fmt.Printf("%d: %v\n", triangle, pfactors)
 	}
 
 }
@@ -29,4 +33,28 @@ func getfactors(n int) []int {
 		}
 	}
 	return factors
+}
+
+// lets see what trianges look like with only prime factors
+
+func primeFilter(factors []int) []int {
+	var primeFactors []int
+	for _, v := range factors {
+		if isPrime(v) {
+			primeFactors = append(primeFactors, v)
+		}
+	}
+	return primeFactors
+}
+
+func isPrime(num int) bool {
+	if num < 2 {
+		return false
+	}
+	for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
 }
