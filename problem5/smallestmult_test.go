b643f6ef9bf49ded3d7320bb7cb5434eb60d4193
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-04-19 23:54:44 -0500

    setting up table based tests
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- problem5/smallestmult_test.go
+++ problem5/smallestmult_test.go
@@ -3,7 +3,19 @@ package main
 import "testing"
 
 func TestSmallestMult(t *testing.T) {
-	t.Run("", func(t *testing.T) {
+	primelimits := []struct {
+		Name string
+		limit int
+		expected []int
+	}{
+		{
+			"get primes up to 1000",
+			1000,
+			[]int{},
+		}
+	}
+
+	t.Run("get primes up to a specified limit", func(t *testing.T) {
 
 	})
 }
